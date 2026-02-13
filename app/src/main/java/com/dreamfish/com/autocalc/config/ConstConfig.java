29a9bde827a2c345cdf2d8ca331e8c5589debe57
al2825613 <172519717+al2825613@users.noreply.github.com>
2026-02-13 04:31:51 -0500

    Initial commit from AutoCalc-master.zip

---
+++ app/src/main/java/com/dreamfish/com/autocalc/config/ConstConfig.java
@@ -0,0 +1,9 @@
+package com.dreamfish.com.autocalc.config;
+
+public class ConstConfig {
+
+  public static final String URL_CHECK_UPDATE = "https://imengyu.top/services/update/?appid=com.dreamfish.com.autocalc&version=";
+  public static final String URL_DOWNLOAD_UPDATE = "https://imengyu.top/services/update/files/AutoCalc-latest.apk";
+  public static final String URL_GET_EXCHANGERATE = "https://imengyu.top/services/exchange-rate/rates";
+
+}
